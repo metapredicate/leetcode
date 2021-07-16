0e30fc25b0a863bdf373d26757dc4ac9aebef5ad
Eoin Dowling <eoin.dowling@ibm.com>
2021-07-16 23:29:30 +0100

    Passed all test cases. Used memoisation

--- 116/827/ProductExceptSelf.py
+++ 116/827/ProductExceptSelf.py
@@ -3,11 +3,14 @@ from operator import mul
 
 class Solution:
     def productExceptSelf(self, nums):
-        result = nums[:]
-        for index, _ in enumerate(nums):
-            temp = nums[:]
-            temp.pop(index)
-            result[index] = reduce(mul, temp, 1)
+        memo = {}
+        result = []
+        for index, value in enumerate(nums):
+            if (value not in memo):
+                temp = nums.pop(index)
+                memo[value] = reduce(mul, nums, 1)
+                nums.insert(index, value)
+            result.append(memo[value])
         return result
 
 if __name__ == '__main__':
