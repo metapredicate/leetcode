32eada5293447498fffa245260b663757527a8d2
Eoin Dowling <eoin.dowling@ibm.com>
2021-07-16 21:04:00 +0100

    reformatting to be consistent Leetcode website.

--- 1647/MinDeletions.py
+++ 1647/MinDeletions.py
@@ -3,19 +3,22 @@
 ############
 from collections import Counter
 
-def minDeletions(s: str) -> int:
-    result = 0
-    freqs = []
-    counter = Counter(s)
-    sorted_counter = dict(sorted(counter.items(), key=lambda item: item[1]))
-    for key, freq in sorted_counter.items():
-        while((freq in freqs) == True):
-            freq -= 1
-            result += 1
-        if(freq >= 1 and (freq not in freqs)):
-            freqs.append(freq)
-    return result
+class Solution:
 
-if(__name__) == '__main__':
-    test = "ceabaacb"
-    print(minDeletions(test))
+    def minDeletions(self, s: str) -> int:
+        result = 0
+        freqs = []
+        counter = Counter(s)
+        sorted_counter = dict(sorted(counter.items(), key=lambda item: item[1]))
+        for key, freq in sorted_counter.items():
+            while((freq in freqs) == True):
+                freq -= 1
+                result += 1
+            if(freq >= 1 and (freq not in freqs)):
+                freqs.append(freq)
+        return result
+
+
+if __name__ == '__main__':
+    foo = Solution()
+    print(foo.minDeletions(""))
